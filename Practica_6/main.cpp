fc2ca99b0da0423442ec6eb4a006bea708c0b892
stefanimp <stefan408.imperato@gmail.com>
2024-01-17 01:20:59 +0100

    Modificación main

--- Practica_6/main.cpp
+++ Practica_6/main.cpp
@@ -146,5 +146,5 @@ int main(int argc, const char * argv[]) {
     for (int i = 0; i < vectAirportsAntequera.size(); ++i) {
         std::cout<<vectAirportsAntequera[i]->getIata() <<std::endl;
     }
-
+    
 }
